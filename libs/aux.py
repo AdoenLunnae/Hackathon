3c8ab70015e634d136db6127f9b56b5f56e671ae
AdoenLunnae <unshekdelmonton@gmail.com>
2018-03-17 19:42:49 +0100

    Añadida función get_info()

--- libs/aux.py
+++ libs/aux.py
@@ -13,3 +13,26 @@ def get_arg(message):
     lst = message.split(' ')
     if len(lst) == 2:
         return lst[1]
+
+
+def get_info(cid, type):
+    if type == 'fiesta':
+        with open('Database/f/%s/base.json' %str(cid), 'r') as file:
+            base=json.load(file)
+            asist=base['asistentes']
+            asiststr=asist.join(', ')
+            fecha=base['fecha']
+            lugar=base['lugar']
+            return 'La fiesta es el {} en {}. Van {}.'.format(fecha, lugar, asiststr)
+
+    elif type == 'quedada':
+        with open('Database/q/%s/base.json' %str(cid), 'r') as file:
+            base = json.load(file)
+            asist = base['asistentes']
+            asiststr = asist.join(', ')
+            fecha = base['fecha']
+            hora = base['hora']
+            lugar = base['lugar']
+            return 'La quedada es el {} a las {} en {}. Van {}.'.format(fecha, hora, lugar, asiststr)
+    else:
+        pass

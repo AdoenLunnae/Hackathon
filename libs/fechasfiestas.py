c6fec5f2dabbec8693f0ca1207e4e0f6154db400
RexusWolf <FrancisJMP99@gmail.com>
2018-03-17 16:00:40 +0100

    Añadidas funciones de fechas

---
+++ libs/fechasfiestas.py
@@ -0,0 +1,12 @@
+import json
+import os
+import shutil
+
+def add_fecha(cid, fecha):
+    with open('Database/f/%s/base.json', %str(cid), 'r') as fechafiestafile:
+        base = json.load(fechafiestafile)
+        base['fecha'].append(fecha)
+
+
+    with open('Database/f/%s/base.json', %str(cid), 'w') as fechafiestafile:
+        json.dump(base, fechafiestafile, indent= 2)
---
+++ libs/fechasquedadas.py
@@ -0,0 +1,12 @@
+import json
+import os
+import shutil
+
+def add_fecha(cid, fecha):
+    with open('Database/q/%s/base.json', %str(cid), 'r') as fechaquedadafile:
+        base = json.load(fechaquedadafile)
+        base['fecha'].append(fecha)
+
+
+    with open('Database/q/%s/base.json', %str(cid), 'w') as fechaquedadafile:
+        json.dump(base, fechaquedadafile, indent= 2)

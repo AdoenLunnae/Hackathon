a90c9d7c754d02b7b00d578b2c0faa0d8c6d1f56
AdoenLunnae <unshekdelmonton@gmail.com>
2018-03-17 18:51:38 +0100

    Corregido un error en get_arg()

--- private.py
+++
@@ -1,7 +0,0 @@
-# !/usr/bin/env python
-# -*- coding:utf-8 -*-
-
-def get_token():
-    return "507603002:AAGp-nejGp56v0yriz3tOUi9OKEV2SwUgG4"
-    # Aquí ponemos el token del bot, este archivo es local, metedlo en el .gitignore
-

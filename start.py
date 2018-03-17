0136f1cbb1cfbed472809fb85f1e141303fa48df
RexusWolf <FrancisJMP99@gmail.com>
2018-03-17 15:21:00 +0100

    Añadida identación en los Json.dump

--- start.py
+++ start.py
@@ -9,7 +9,7 @@ os.mkdir('Database/q')
 os.mkdir('Database/f')
 with open('Database/quedadas.json', 'w') as f:
     data = {'list_quedadas': []}
-    json.dump(data, f)
+    json.dump(data, f, indent = 2)
 with open('Database/fiestas.json', 'w') as f:
     data = {'list_fiestas': []}
-    json.dump(data, f)
+    json.dump(data, f, indent = 2)

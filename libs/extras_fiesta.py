0e5229fcb2d65291a00afa447263e56772501652
Pablo <i72rogup@uco.es>
2018-03-17 18:50:30 +0100

    Comentarios añadidos en extras_fiesta

--- libs/extras_fiesta.py
+++ libs/extras_fiesta.py
@@ -1,5 +1,9 @@
 import json
 
+#FUNCIONES PARA LA LISTA DE LA COMPRA
+
+#La siguiente función añade un item a la lista de la compras
+
 def add_item_compra(cid, item):
     with open('Database/f/%s/compra.json' %str(cid), 'r') as itemcompra:
         compras = json.load(itemcompra)
@@ -25,6 +29,11 @@ def del_item_compra(cid, item):
     with open('Database/f/%s/compra.json' %str(cid), 'r') as itemcompra:
         json.dump(compras, itemcompra, indent = 2)
 
+
+#FUNCIONES PARA LA LISTA DE PETICIONES
+
+#Función para añadir una petición
+
 def add_peticion(cid, uid, peticion):
     with open('Database/f/%s/peticiones.json' %str(cid), 'r') as peticionesfile:
         peticiones = json.load(peticionesfile)
@@ -32,6 +41,8 @@ def add_peticion(cid, uid, peticion):
 
     with open('Database/f/%s/peticiones.json' %str(cid), 'r') as peticionesfile:
         json.dump(peticiones, peticionesfile, indent = 2)
+
+#Función que retorna una cadena con todas las peticiones de un usuario en un formato BONITO
 
 def print_peticiones(cid, uid):
     with open('Database/f/%s/peticiones.json' %str(cid), 'r') as peticionesfile:

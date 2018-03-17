631ee12871042b395f9193b59352bdce955897f3
Pablo <i72rogup@uco.es>
2018-03-17 16:37:03 +0100

    AsistentesQuedadas añadido

---
+++ libs/AsistentesQuedadas.py
@@ -0,0 +1,22 @@
+def add_asistente_quedadas(cid, uid):
+    with open('Database/q/%s/base.json' %str(cid), 'r') as basefile:
+        base = load.json(basefile)
+        base['asistentes'].append(uid)
+
+    with open('Database/q/%s/base.json' %str(cid), 'w') as basefile:
+        json.dump(base, basefile, indent = 2)
+
+def exist_asistente_quedadas(cid, uid):
+    with open('Database/q/%s/base.json' %str(cid), 'r') as basefile:
+        base = load.json(basefile)
+        for userid in base['asistentes']:
+            if userid == uid: return True
+        else: False
+
+def delete_asistente_quedadas(cid, uid):
+    with open('Database/q/%s/base.json' %str(cid), 'r') as basefile:
+        base = load.json(basefile)
+        base['asistentes'].remove(uid)
+
+    with open('Database/q/%s/base.json' %str(cid), 'r') as basefile:
+        jason.dump(base, basefile, indent = 2)

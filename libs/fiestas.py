66d6ed748ae6d139aa7f23902d97867447bb656a
RexusWolf <FrancisJMP99@gmail.com>
2018-03-17 13:54:18 +0100

    Terminado fiestas.py

--- libs/fiestas.py
+++ libs/fiestas.py
@@ -2,15 +2,31 @@ import json
 import os
 import shutil
 
+
+
+def add_fiesta(cid):
+    with open('Database/fiestas.json', 'r') as fiestasfile:
+        id_fiesta_chat = json.load(fiestasfile)
+        id_fiesta_chat['list_fiestas'].append(cid)
+
+    with open('Database/fiestas.json', 'w') as fiestasfile:
+        json.dump(id_fiesta_chat, fiestasfile)
+
+    os.mkdir('Database/f/%s' %str(cid))
+
+    with open('Database/f/%s/base.json' %str(cid), 'w') as basefile:
+        data = {"asistentes" : [], "lugar" : "", "fecha" : ""}
+        json.dump(data, basefile)
+
 #Para borrar una fiesta primero debemos comprobar si existe una fiesta creada en el chat.
 
 def exist_fiesta(cid):
-    with open('Database/fiestas.json', 'r') as jsonfile:
-        fiestas = json.load(jsonfile)['list_fiestas']
+    with open('Database/fiestas.json', 'r') as fiestasfile:
+        fiestas = json.load(fiestasfile)['list_fiestas']
         if cid in fiestas:
-                    return True
+            return True
         else:
-                    return False
+            return False
 
 #Si existe la fiesta, la borraremos.
 
@@ -19,12 +35,12 @@ def delete_fiesta(cid):
         fiestas = json.load(fiestasfile)
 
         for id_fiesta_chat in fiestas['list_fiestas']:
-            if cid == int(fiesta.id):
+            if cid == int(id_fiesta_chat):
                 fiestas['list_fiestas'].remove(id_fiesta_chat)
 
 #Sobreescribimos el diccionario.
 
-    with open('Database/fiestas.py', 'w') as fiestasfile:
+    with open('Database/fiestas.json', 'w') as fiestasfile:
         json.dump(fiestas, fiestasfile)
 
 #Borramos la carpeta creada al crear una fiesta, con sus archivos.
